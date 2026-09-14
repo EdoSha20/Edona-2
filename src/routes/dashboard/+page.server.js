ac61cd4faec3042aa3aa2b1f9f44713c1d616f39
Dona-aa <edonaperaj4@gmail.com>
2026-09-14 10:31:15 +0200

    implement pdf upload

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -1,5 +1,8 @@
 import db from '$lib/server/db';
 import { redirect } from '@sveltejs/kit';
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import { fail, redirect } from '@sveltejs/kit';
 
 export async function load({ cookies }) {
 
@@ -40,3 +43,66 @@ export async function load({ cookies }) {
         pdfs
     };
 }
+
+export const actions = {
+
+    default: async ({ request, cookies }) => {
+
+        const userId = cookies.get('userId');
+
+        if (!userId) {
+            throw redirect(303, '/login');
+        }
+
+        const data = await request.formData();
+
+        const file = data.get('pdf');
+
+        // Check that a file was selected
+        if (!file || file.size === 0) {
+            return fail(400, {
+                error: 'Keine Datei ausgewählt'
+            });
+        }
+
+        // Only allow PDFs
+        if (file.type !== 'application/pdf') {
+            return fail(400, {
+                error: 'Nur PDF-Dateien sind erlaubt'
+            });
+        }
+
+        // Create unique filename
+        const savedFilename =
+            Date.now() + '-' + file.name;
+
+        const filepath = path.join(
+            'static',
+            'uploads',
+            savedFilename
+        );
+
+        // Convert uploaded PDF into bytes
+        const buffer =
+            Buffer.from(await file.arrayBuffer());
+
+        // Save actual PDF
+        await writeFile(filepath, buffer);
+
+        // Save information inside MySQL
+        await db.execute(
+            `INSERT INTO pdfs
+             (filename, filepath, user_id)
+             VALUES (?, ?, ?)`,
+            [
+                file.name,
+                '/uploads/' + savedFilename,
+                userId
+            ]
+        );
+
+        return {
+            success: true
+        };
+    }
+};
