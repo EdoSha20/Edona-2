f05a6d178e24000f9bb0f86f39241af67cfeac42
Dona-aa <edonaperaj4@gmail.com>
2026-09-14 09:56:15 +0200

    Create user dashboard layout

---
+++ src/routes/dashboard/+page.server.js
@@ -0,0 +1,19 @@
+<h1>Meine PDFs</h1>
+
+<h2>PDF hochladen</h2>
+
+<form>
+    <input
+        type="file"
+        name="pdf"
+        accept="application/pdf"
+    />
+
+    <button type="submit">
+        Hochladen
+    </button>
+</form>
+
+<h2>Meine Dateien</h2>
+
+<p>Noch keine PDFs vorhanden.</p>
