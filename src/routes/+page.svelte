c3da9c6dd4b5287dc79b37e66b1ad9be4b3d9d15
Edona Shabaj <edonashabaj@Edonas-MacBook-Air.local>
2026-09-14 10:37:01 +0200

    create public marketing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+
+    <h1>PDF Manager</h1>
+
+    <p>
+        Speichere und verwalte deine PDFs einfach online.
+    </p>
+
+    <a href="/login">
+        Zum Login
+    </a>
+
+</main>
